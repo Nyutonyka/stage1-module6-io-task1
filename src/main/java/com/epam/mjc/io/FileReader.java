6d790515db97318ac96ea098a6feeeb8d35822e2
nyuto <nuytonyka@gmail.com>
2023-04-26 18:58:35 +0300

    Solved

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,41 +1,44 @@
 package com.epam.mjc.io;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 
 public class FileReader {
-
 
     public Profile getDataFromFile(File file) {
         Profile profile = null;
 
         try (BufferedReader br = new BufferedReader(new java.io.FileReader(file))) {
             String line;
+            String name = null;
+            int age = 0;
+            String email = null;
+            Long phone = null;
             while ((line = br.readLine()) != null) {
                 String[] parts = line.split(":");
                 switch (parts[0]) {
                     case "Name":
-                        profile = new Profile(parts[1].trim(), 0, null, null);
+                        name = parts[1].trim();
                         break;
                     case "Age":
-                        profile.setAge(Integer.parseInt(parts[1].trim()));
+                        age = Integer.parseInt(parts[1].trim());
                         break;
                     case "Email":
-                        profile.setEmail(parts[1].trim());
+                        email =parts[1].trim();
                         break;
                     case "Phone":
-                        profile.setPhone(Long.parseLong(parts[1].trim()));
+                        phone = Long.parseLong(parts[1].trim());
                         break;
                     default:
                         System.out.println("Invalid input.");
                         break;
                 }
             }
-
+            profile = new Profile(name, age, email, phone);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
-      // System.out.println(profile);
         return profile;
     }
 }
